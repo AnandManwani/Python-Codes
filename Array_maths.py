3a6763f76b6d173f7845a69e45699b23fa5c73a1
AnandManwani <83331735+AnandManwani@users.noreply.github.com>
2021-05-18 13:43:31 +0530

    Add files via upload

---
+++ Array_maths.py
@@ -0,0 +1,15 @@
+import numpy
+
+
+
+n, m = [int(x) for x in input().strip().split()]
+
+a = numpy.array([[int(x) for x in input().strip().split()] for _ in range(n)])
+b = numpy.array([[int(x) for x in input().strip().split()] for _ in range(n)])
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+print(a**b)
